a490e15f248216f6ce2f2cdcc5875c4b13094ab3
GerkHard <gerkhard.biz@gmail.com>
2019-10-09 11:52:02 +0300

    Render Blocks, move Platform

--- game.js
+++ game.js
@@ -2,28 +2,50 @@ let game = {
    ctx: null,
    platform: null,
    ball: null,
+   blocks: [],
+   rows: 4,
+   cols: 8,
    sprites: {
       bg: null, // null, потому что в этих свойствах мы ожидаем получить объект изображения
       ball: null,
-      platform: null
+      platform: null,
+      block: null
    },
    init: function() {
       // init
       this.ctx = document.querySelector('#gameInterface').getContext('2d');
+      this.setEvents();
+   },
+   setEvents() {
+      window.addEventListener('keydown', e => {
+         if (e.keyCode === 37) { // left
+            this.platform.dx = -this.platform.velocity;
+         } else if (e.keyCode === 39) {
+            this.platform.dx = this.platform.velocity;
+         }
+      });
+
+      window.addEventListener('keyup', e => {
+         if (e.keyCode === 37 || e.keyCode === 39) {
+            this.platform.dx = 0;
+         }
+      });
    },
    preload(callback) {
       // preload
       let loaded = 0;
       let required = Object.keys(this.sprites).length; // разобраться
+      let onImgLoad = () => {
+         ++loaded;
+         if (loaded >= required) {
+            callback(); // ?
+         }
+      };
+
       for (let key in this.sprites) { // ?
          this.sprites[key] = new Image();
          this.sprites[key].src = `./img/${key}.png`;
-         this.sprites[key].addEventListener('load', () => {
-            ++loaded;
-            if (loaded >= required) {
-               callback();
-            }
-         });
+         this.sprites[key].addEventListener('load', onImgLoad);
       }
       
       /*this.background = new Image();
@@ -35,10 +57,33 @@ let game = {
       this.platform = new Image();
       this.platform.src = './img/ball.png';*/
    },
+   createLevel() {
+      for (let row = 0; row < this.rows; row++) {
+         for (let col = 0; col < this.cols; col++) {
+            this.blocks.push({
+               x: 64 * col + 65,
+               y: 24 * row + 35
+            });
+         }
+      }
+   },
+   update() { // обновление игрового состояния
+      this.platform.move();
+   },
    run() {
       // run
       window.requestAnimationFrame(() => {
+         this.update();
          this.render();
+         this.run();
+      });
+   },
+   renderBlocks() {
+      /*for (let block of this.blocks) {
+         this.ctx.drawImage(this.sprites.block, block.x, block.y);
+      }*/
+      this.blocks.forEach(block => {
+         this.ctx.drawImage(this.sprites.block, block.x, block.y);
       });
    },
    render() {
@@ -46,10 +91,12 @@ let game = {
       this.ctx.drawImage(this.sprites.bg, 0, 0); // метод, который указывает контексту, что мы планируем отрисовать, 2-й и 3-й пареметры - координаты (левый верхний угол)
       this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
       this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+      this.renderBlocks();
    },
    start: function() {
       this.init();
       this.preload(() => {
+         this.createLevel();
          this.run();
       });
       
@@ -64,8 +111,15 @@ game.ball = {
 };
 
 game.platform = {
+   velocity: 6, // velocity for platform
+   dx: 0, // current change for platform
    x: 280,
-   y: 300
+   y: 300,
+   move() {
+      if (this.dx) { // проверяем движется ли платформа
+         this.x += this.dx;
+      }
+   }
 };
 
 window.addEventListener('load', () => {
