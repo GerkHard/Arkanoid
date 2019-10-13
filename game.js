5a770a7e1fa1574fc7ebe1b86eea20a7b9476d90
GerkHard <gerkhard.biz@gmail.com>
2019-10-13 18:05:51 +0300

    Ball with Platform, Ball Move, RandomBall Move

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
+const KEYS = {
+   LEFT: 37,
+   RIGHT: 39,
+   SPACE: 32
+};
+
 let game = {
    ctx: null,
    platform: null,
@@ -5,6 +11,8 @@ let game = {
    blocks: [],
    rows: 4,
    cols: 8,
+   width: 640,
+   height: 360,
    sprites: {
       bg: null, // null, потому что в этих свойствах мы ожидаем получить объект изображения
       ball: null,
@@ -18,16 +26,16 @@ let game = {
    },
    setEvents() {
       window.addEventListener('keydown', e => {
-         if (e.keyCode === 37) { // left
-            this.platform.dx = -this.platform.velocity;
-         } else if (e.keyCode === 39) {
-            this.platform.dx = this.platform.velocity;
+         if (e.keyCode === KEYS.SPACE) {
+            this.platform.fire();
+         } else if (e.keyCode === KEYS.LEFT || e.keyCode === KEYS.RIGHT) {
+            this.platform.start(e.keyCode);
          }
       });
 
       window.addEventListener('keyup', e => {
-         if (e.keyCode === 37 || e.keyCode === 39) {
-            this.platform.dx = 0;
+         if (e.keyCode === KEYS.LEFT || e.keyCode === KEYS.RIGHT) {
+            this.platform.stop();
          }
       });
    },
@@ -69,6 +77,7 @@ let game = {
    },
    update() { // обновление игрового состояния
       this.platform.move();
+      this.ball.move();
    },
    run() {
       // run
@@ -88,6 +97,7 @@ let game = {
    },
    render() {
       // render
+      this.ctx.clearRect(0, 0, this.width, this.height);
       this.ctx.drawImage(this.sprites.bg, 0, 0); // метод, который указывает контексту, что мы планируем отрисовать, 2-й и 3-й пареметры - координаты (левый верхний угол)
       this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x, this.ball.y, this.ball.width, this.ball.height);
       this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
@@ -100,14 +110,32 @@ let game = {
          this.run();
       });
       
+   },
+   random(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
    }
 };
 
 game.ball = {
+   velocity: 3,
+   dx: 0,
+   dy: 0,
    x: 320,
    y: 280,
    width: 20,
-   height: 20
+   height: 20,
+   start() {
+       this.dy = this.velocity;
+       this.dx = game.random(-this.velocity, this.velocity); // в диапазоне от -3 до +3
+   },
+   move() {
+      if (this.dy) {
+         this.y -= this.dy;
+      }
+      if (this.dx) {
+         this.x += this.dx;
+      }
+   }
 };
 
 game.platform = {
@@ -115,9 +143,29 @@ game.platform = {
    dx: 0, // current change for platform
    x: 280,
    y: 300,
+   ball: game.ball,
+   fire() {
+      if (this.ball) { // запускать мячь мы можем только пока он есть на платформе, принадлежит этой платформе
+         this.ball.start();
+         this.ball = null;
+      }
+   },
+   start(direction) {
+      if (direction === KEYS.LEFT) {
+         this.dx = -this.velocity;
+      } else if (direction === KEYS.RIGHT) {
+         this.dx = this.velocity;
+      }
+   },
+   stop() {
+      this.dx = 0;
+   },
    move() {
       if (this.dx) { // проверяем движется ли платформа
          this.x += this.dx;
+         if (this.ball) {
+            this.ball.x += this.dx;
+         }
       }
    }
 };
